86a0de91fc120e22dc1cbda4d36c55b16e941d5b
adminseverkeyvip <phantam0003873@gmail.com>
2026-08-07 07:29:11 +0700

    Update app.py

--- app.py
+++ app.py
@@ -121,6 +121,7 @@ def check_player():
         return jsonify({ "message": f"Invalid response: {str(e)}"}), 500
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 10000))
+    app.run(host='0.0.0.0', port=port, debug=False)
 # Given By zexxyh4x 
 # Given By zexxyh4x 
